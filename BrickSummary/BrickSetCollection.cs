42946dc85ab244de082cf3939008bc72d3b73236
Eilon Lipton <Eilon@users.noreply.github.com>
2021-11-07 23:38:23 -0800

    Add set analysis

--- BrickSummary/BrickSetCollection.cs
+++ BrickSummary/BrickSetCollection.cs
@@ -4,6 +4,7 @@
     {
         public string? Name { get; set; }
         public List<BrickSetItem> Sets { get; } = new();
+        public Dictionary<string, string> Analysis { get; } = new();
 
         public async Task LoadSets(Stream csvStream)
         {
